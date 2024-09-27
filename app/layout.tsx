e3efb396f4408a1086e4f8387088c24a07dd2ad1
maaz2022 <maaznazeer098@gmail.com>
2024-09-27 23:01:25 +0500

    linting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Nunito } from "next/font/google";
 const nunito = Nunito({ subsets: ["latin"], weight:["300","400","500","600","800"] });
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
